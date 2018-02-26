ae3dff4e37b9d32275e6aa5d8e538444c4bb6ad2
Artem Korotchenko <clifredd@gmail.com>
2018-02-26 10:59:44 +0500

    Modified Users table (forgot to add :unconfirmed_email field)

--- db/schema.rb
+++ db/schema.rb
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 20180226045505) do
     t.string "confirmation_token"
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
+    t.string "unconfirmed_email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
