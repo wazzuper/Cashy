b9f9b2485f4ae65b089a3300410316d563ac401e
Artem Korotchenko <clifredd@gmail.com>
2018-02-26 14:14:48 +0500

    configured smtp for production, added simple log in/log out if condition

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,4 +91,15 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: 'smtp.gmail.com',
+    port: 587,
+    domain: 'gmail.com',
+    authentication: 'plain',
+    user_name: ENV['GMAIL_USERNAME'],
+    password: ENV['GMAIL_PASSWORD'],
+    enable_starttls_auto: true
+  }
 end
