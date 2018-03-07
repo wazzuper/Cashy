f8f3626648bdc9dacf8e1ee5bb2f6a460d74d4ec
Artem Korotchenko <clifredd@gmail.com>
2018-03-07 21:45:15 +0500

    Modified omniauth service

--- app/services/omni_auth_service.rb
+++ app/services/omni_auth_service.rb
@@ -14,18 +14,46 @@ class OmniAuthService
   delegate :provider, :uid, to: :auth_params
 
   def user
-    user = User.where(provider: provider, uid: uid).first
-    user ||= User.new(
-      provider: provider,
-      uid: uid,
-      email: auth_params.info.email,
-      password: Devise.friendly_token[0, 20]
-    )
-    create_user(user)
+    user = Identity.where(provider: provider, uid: uid).first
+    unless user.nil?
+      user.user
+    else
+      registered_user = User.where(email: auth_params.info.email).first
+      unless registered_user.nil?
+        Identity.new(
+          provider: provider,
+          uid: uid,
+          user_id: registered_user.id
+        )
+        register_user(registered_user)
+      else
+        user = User.new(
+          email: auth_params.info.email,
+          password: Devise.friendly_token[0, 20]
+        )
+        user_provider = Identity.new(
+          provider: provider,
+          uid: uid,
+          user_id: user.id
+        )
+        create_provider(user_provider)
+        create_user(user)
+      end
+    end
   end
 
   def create_user(user)
     user.skip_confirmation!
+    user.save
+    user
+  end
+
+  def create_provider(provider)
+    provider.save
+    provider
+  end
+
+  def register_user(user)
     user.save
     user
   end
