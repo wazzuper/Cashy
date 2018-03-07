f18873f656b44c95862d51b3a37c0e41e322c244
Artem Korotchenko <clifredd@gmail.com>
2018-03-07 23:49:04 +0500

    More refactoring of omniauth service

--- app/services/omni_auth_service.rb
+++ app/services/omni_auth_service.rb
@@ -14,24 +14,32 @@ class OmniAuthService
   delegate :provider, :uid, to: :auth_params
 
   def user
-    user = Identity.where(provider: provider, uid: uid).first
-    unless user.eql?(nil)
-      user.user
+    existing_provider = Identity.where(provider: provider, uid: uid).first
+    unless existing_provider.eql?(nil)
+      existing_provider.user
     else
-      user = User.where(email: email).first_or_create! do |user|
-        user.password = Devise.friendly_token[0, 20]
-        user.skip_confirmation!
-      end
-      Identity.create!(
-        provider: provider,
-        uid: uid,
-        user_id: user.id
-      )
-      user
+      create_or_return_user(provider, uid)
     end
+  end
+
+  def create_or_return_user(provider, uid)
+    user = User.where(email: email).first_or_create! do |user|
+      user.assign_attributes(password: password)
+      user.skip_confirmation!
+    end
+    Identity.create!(
+      provider: provider,
+      uid: uid,
+      user_id: user.id
+    )
+    user
   end
 
   def email
     auth_params.info.email
+  end
+
+  def password
+    Devise.friendly_token[0, 20]
   end
 end
