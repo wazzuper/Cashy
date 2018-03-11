e717c781dc6d47ab455ebbd5254f375e2aa85905
Artem Korotchenko <clifredd@gmail.com>
2018-03-11 15:44:06 +0500

    Refactoring in omniauth controller and service

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -1,29 +1,28 @@
 class OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def github
-    if user.persisted?
-      sign_in_and_redirect user, event: :authentication
-      flash[:notice] = I18n.t('devise.omniauth_callbacks.success', kind: 'GitHub')
-    else
-      return_to_registration_page
-    end
+    handle_oauth_callback
   end
 
   def google_oauth2
-    if user.persisted?
-      sign_in_and_redirect user, event: :authentication
-      flash[:notice] = I18n.t('devise.omniauth_callbacks.success', kind: 'Google')
-    else
-      return_to_registration_page
-    end
+    handle_oauth_callback
   end
 
   private
 
-  def user
-    @user ||= OmniAuthService.new(request.env['omniauth.auth']).get_user
+  def handle_oauth_callback
+    if user.persisted?
+      sign_in_and_redirect user, event: :authentication
+      flash[:notice] = I18n.t('devise.omniauth_callbacks.success', kind: provider)
+    else
+      redirect_to new_user_registration_url
+    end
   end
 
-  def return_to_registration_page
-    redirect_to new_user_registration_url, alert: user.errors.full_messages.join("")
+  def user
+    @user ||= OmniAuthService.new(request.env['omniauth.auth']).user
+  end
+
+  def provider
+   request.env['omniauth.auth'].provider.split('_')[0].humanize
   end
 end
--- app/services/omni_auth_service.rb
+++ app/services/omni_auth_service.rb
@@ -5,29 +5,25 @@ class OmniAuthService
     @auth_params = auth_params
   end
 
-  def get_user
-    user
+  def user
+    existing_provider = Identity.find_by(provider: provider, uid: uid)
+    if existing_provider.eql?(nil)
+      create_or_return_user(provider, uid)
+    else
+      existing_provider.user
+    end
   end
 
   private
 
   delegate :provider, :uid, to: :auth_params
 
-  def user
-    existing_provider = Identity.where(provider: provider, uid: uid).first
-    unless existing_provider.eql?(nil)
-      existing_provider.user
-    else
-      create_or_return_user(provider, uid)
-    end
-  end
-
   def create_or_return_user(provider, uid)
     user = User.where(email: email).first_or_create! do |user|
       user.assign_attributes(password: password)
       user.skip_confirmation!
     end
-    Identity.create!(
+    user.identities.create!(
       provider: provider,
       uid: uid,
       user_id: user.id
