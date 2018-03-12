d0867de681e09c3ea43d701cc1afb3be7a6d5d6e
Artem Korotchenko <clifredd@gmail.com>
2018-03-12 13:08:48 +0500

    Added users factory, login macros (just in case)

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,4 +1,8 @@
 FactoryBot.define do
   factory :user do
+    sequence(:email) { |n| "user#{n}@email.com" }
+    password 'secret123!@#'
+    password_confirmation 'secret123!@#'
+    confirmed_at Time.zone.today
   end
 end
