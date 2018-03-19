15ff0fec3203c14b6d69cd7a48f0f632333614b0
Artem Korotchenko <clifredd@gmail.com>
2018-03-19 14:32:48 +0500

    Rubocop refactoring

--- spec/controllers/transactions_controller_spec.rb
+++ spec/controllers/transactions_controller_spec.rb
@@ -11,15 +11,13 @@ RSpec.describe TransactionsController, type: :controller do
   describe '#create' do
     context 'with valid attributes' do
       it 'saves the new transaction in the database' do
-        expect { post :create, params: { transaction: attributes_for(:transaction) } }.to
-                change(Transaction, :count).by(1)
+        expect { post :create, params: { transaction: attributes_for(:transaction) } }.to change(Transaction, :count).by(1)
       end
     end
 
     context 'with invalid attributes' do
       it 'does not save the transaction' do
-        expect { post :create, params: {transaction: attributes_for(:invalid_transaction) } }.to_not
-                change(Transaction, :count)
+        expect { post :create, params: {transaction: attributes_for(:invalid_transaction) } }.to_not change(Transaction, :count)
       end
 
       it 'redirects to main page' do
