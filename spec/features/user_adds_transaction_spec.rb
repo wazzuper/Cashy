364e624bcf685689cbfa09b08f8ca6701b41c98c
Artem Korotchenko <clifredd@gmail.com>
2018-03-19 13:56:33 +0500

    Modified feature test

--- spec/features/user_adds_transaction_spec.rb
+++ spec/features/user_adds_transaction_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-feature 'Main Page Visitation' do
+feature 'Adding transaction' do
   let(:user) { create(:user) }
 
   before do
@@ -9,18 +9,31 @@ feature 'Main Page Visitation' do
 
   scenario 'allows a user to create a transaction' do
     fill_in('Amount', with: 555)
-    find('select#transaction_date_3i').find("option[value='#{15}']").select_option
-    find('select#transaction_date_2i').find("option[value='#{3}']").select_option
-    find('select#transaction_date_1i').find("option[value='#{2018}']").select_option
+    fill_in('Date', with: '15/3/2018')
     click_button(I18n.t 'main_page_user.form_add')
 
     expect(page).to have_content(I18n.t 'transaction.added')
     expect(page).to have_content(555)
+    expect(page).to have_content('15/03/2018')
   end
 
   scenario 'outputs an error message for user because of empty amount value' do
     click_button(I18n.t 'main_page_user.form_add')
 
     expect(page).to have_content('Amount can\'t be blank')
+  end
+
+  scenario 'outputs an error message for user because he type a string' do
+    fill_in('Amount', with: '+-eee')
+    click_button(I18n.t 'main_page_user.form_add')
+
+    expect(page).to have_content('Amount is not a number')
+  end
+
+  scenario 'outputs an error message for user because he choose date in the future' do
+    fill_in('Date', with: '15/3/2222')
+    click_button(I18n.t 'main_page_user.form_add')
+
+    expect(page).to have_content('Date can\'t be blank')
   end
 end
